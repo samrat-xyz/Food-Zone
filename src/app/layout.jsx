f37fb9aa79706428652068d066652bf3c0fbf6ef
Asif-vai <mr3366533@gmail.com>
2025-12-15 18:00:01 +0600

    maked header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/shared/Header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>
+          <Header></Header>
+        </header>
         {children}
       </body>
     </html>
---
+++ src/components/shared/Header/Header.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  const links = (
+    <>
+      <li>
+        <Link href='/foods' className="btn">Foods</Link>
+      </li>
+      <li>
+        <Link href='/reviews' className="btn">Reviews</Link>
+      </li>
+    </>
+  );
+  return (
+    <nav className="flex items-center justify-between border p-6 bg-[#3D240C]">
+      <p className="text-3xl font-semibold text-yellow-500">FoodZone</p>
+      <ul className="flex items-center gap-4 ">{links}</ul>
+    </nav>
+  );
+}
+
+export default Header;
